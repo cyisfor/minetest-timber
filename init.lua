49582ac2dfe4d6ee3c6927acf67aa7f82903fcfd
user <nobody>
2016-05-03 20:08:53 +0000

    add drops
    also some sort of set thing, just to be pretty

--- init.lua
+++ init.lua
@@ -13,54 +13,68 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
+function set(list)
+   local s = {}
+   for _,l in ipairs(list) do s[l] = true end
+   return s
+end
+
 -- this mod is based on http://minetest.net/forum/viewtopic.php?id=1590
 local timber = {}
-timber.nodenames={"default:papyrus", "default:cactus", "group:tree"}
+timber.nodenames=set {"default:papyrus", "default:cactus", "group:tree"}
 timber.search = 3 -- how far to look for suspended trunks
 timber.limit = 100 -- how many iterations before give up removing suspended
 
-function dig_node(np) 
-   minetest.env:remove_node(np)
-   for _, item in ipairs(minetest.get_node_drops(name)) do
+function timber.dig_node(np,node) 
+   core:remove_node(np)
+   for _, item in ipairs(minetest.get_node_drops(node.name)) do
 	  local tp = {x = np.x + math.random()/2 - 0.25, y = np.y, z = np.z + math.random()/2 - 0.25}
-	  minetest.env:add_item(tp, item)
+	  core:add_item(tp, item)
    end
 end
 
-function dig_air_trees(center, node, count)
-   dig_node(np)
+function timber.dig_air_trees(center, node, count)
+   timber.dig_node(center,node)
    count = count + 1
    while count < timber.limit do
-	  local np = minetest.env:find_node_near(center, timber.search, node)
+	  local np = core:find_node_near(timber.search, center, node.name)
 	  if np == nil then
 		 break
 	  end
 	  local below = minetest.get_node_or_nil({np.X,np.Y-1,np.Z})
 	  if below ~= nil and
 	  not minetest.registered_nodes[below.name].walkable then
-		 count += dig_air_trees(np, node, count+1)
+		 count = count + dig_air_trees(np, node, count+1)
 	  end
    end
    return count
 end
 
-function dig_up(pos, nodes)
+function timber.same_node(np,nodes)
+   local node = core:get_node_or_nil(np)
+   if node == nil then return nil end
+   if nodes[node.name] then return node end
+   for _,group in ipairs(minetest.registered_nodes[node.name].groups) do
+	  print("group",group)
+	  if nodes[group] then return node end
+   end
+   return nil
+end
+
+
+function timber.dig_up(pos, node)
+   assert pos.X
    local height = nil
    -- check up first, so it doesn't get dug by air trees
-   for height = 1,100 do
+   for derp = 1,100 do
+	  height = derp
 	  local np = {pos.x,pos.y+height,pos.z}
-	  if nil == minetest.env:find_node_near(np,1,nodes) then
-		 break
-	  end
-	  dig_node(np)
+	  timber.dig_node(np,node)
    end
    for i = 1,height do
 	  local np = {pos.x,pos.y+height,pos.z}
-	  dig_air_trees(np, nodes, 0)
+	  timber.dig_air_trees(np, node, 0)
    end
 end
 
-minetest.register_on_dignode(function(pos, node)
-	  dig_up(pos,timber.nodenames)
-end)
-
+minetest.register_on_dignode(timber.dig_up)
